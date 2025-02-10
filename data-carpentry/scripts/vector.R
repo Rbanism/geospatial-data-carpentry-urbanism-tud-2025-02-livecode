1d7582c92c0af4b9067005bfd22b558e6089b4bd
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-10 14:43:26 +0100

    gitautosync update 22

--- data-carpentry/scripts/vector.R
+++ data-carpentry/scripts/vector.R
@@ -89,4 +89,5 @@ st_geometry_type(motorway_Delft_summary)
 road_types <- c("motorway", "primary", "secondary", "cycleway")
 
 lines_Delft_selection <- lines_Delft %>%
-  filter(highway %in% road_types)
+  filter(highway %in% road_types) %>%
+  

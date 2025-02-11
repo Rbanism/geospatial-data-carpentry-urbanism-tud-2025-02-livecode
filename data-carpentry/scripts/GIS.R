58c367cb97d5a842e846381c4c9a12ec77284383
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 14:05:16 +0100

    gitautosync update 65

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -29,5 +29,5 @@ buildings$build_date <-if_else(start_date < 1900,
 
 ggplot(data = buildings) +
   geom_sf(aes(fill = build_date, colour = build_date)) +
-  scale_fill_viridis_c()
+  scale_fill_viridis_c(option)
 

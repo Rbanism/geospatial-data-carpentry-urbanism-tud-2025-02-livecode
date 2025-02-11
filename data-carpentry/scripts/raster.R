52fbe3174e0397015a8d8ca6e70c85afb2143011
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 11:55:22 +0100

    gitautosync update 278

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -166,5 +166,6 @@ describe("data/tudlib-rgb.tif")
 RGB_band1_TUD <- rast("data/tudlib-rgb.tif", lyrs = 1)
 RGB_band1_TUD_df <- as.data.frame(RGB_band1_TUD, xy = TRUE)
 
-
+ggplot() +
+  geom_raster()
 

0e46aec855b1a127d98c9e9a25649767b6d4ec4b
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 14:03:38 +0100

    gitautosync update 61

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -28,5 +28,6 @@ buildings$build_date <-if_else(start_date < 1900,
                                1900, start_date)
 
 ggplot(data = buildings) +
-  geom_sf(aes(fill = build_date, colour = build_date))
+  geom_sf(aes(fill = build_date, colour = build_date)) +
+
 

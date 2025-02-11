d2764afe5b7251b34e9ddb4fcb672738c956d8e4
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 11:13:56 +0100

    gitautosync update 206

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -118,4 +118,5 @@ res(DTM_TUD)
 DTM_hill_EPSG28992_TUD <- project(DTM_hill_TUD,
                                   crs(DTM_TUD),
                                   res = res(DTM_TUD))
-res(DTM_hill_EPSG)
+res(DTM_hill_EPSG28992_TUD)
+

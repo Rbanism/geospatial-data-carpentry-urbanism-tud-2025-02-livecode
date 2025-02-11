6754ee17bae4ba4b1d6323c5d03c27f6b76d6ee9
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 09:38:09 +0100

    gitautosync update 40

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -15,4 +15,5 @@ DSM_TUD_df <- as.data.frame(DSM_TUD, xy = TRUE)
 str(DSM_TUD_df)
 
 ggplot() +
-geom_raster(data = DSM_TUD_df, aes(x = x, y = y, fill = 'tud-dsm-5m'))
+geom_raster(data = DSM_TUD_df, aes(x = x, y = y, fill = 'tud-dsm-5m')) +
+scale_fill_viri

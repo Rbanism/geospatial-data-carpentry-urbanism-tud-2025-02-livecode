466c6cced77f1a6ca00941d8f09fe6814b758afd
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 14:24:38 +0100

    gitautosync update 86

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -59,7 +59,6 @@ replication_function <- function(cityname, year=1900){
     ggtitle(paste0("Old buildings in ",cityname)) +
     coord_sf(datum = st_crs(28992))
 
-
 }
 
 replication_function("Brielle, NL")

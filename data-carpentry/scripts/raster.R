878c71cdfff7b7b0bfdf391b6511f6012196eea1
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 10:07:03 +0100

    gitautosync update 93

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -44,4 +44,4 @@ custom_bins <- c(-10, 0, 5, 100)
 DSM_TUD_df <- DSM_TUD_df %>%
   mutate(fct_elevation_cb = cut(`tud-dsm-5m`, breaks = custom_bins))
 
-levels(DSM_TUD_df$)
+levels(DSM_TUD_df$fct_elevation_cb)

406f275d8821ce2780244d6ff5743c3b15ac6b2d
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 11:43:26 +0100

    gitautosync update 251

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -150,4 +150,6 @@ custom_bins <- c(-5, 0, 10, 20, 30, 100)
 CHM_TUD_df <- CHM_TUD_df %>%
 mutate(canopy_discrete = cut(`tud-dsm-5m`, breaks = custom_bins))
 ggplot() +
-  geom_raster(data = CHM_TUD_df, aes())
+  geom_raster(data = CHM_TUD_df, aes(x =x, y = y,
+                                     fill = canopy_discrete)) +
+

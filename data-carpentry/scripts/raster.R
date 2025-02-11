061272145395ca81a76db17cc5cb30eedc069d4f
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 10:16:52 +0100

    gitautosync update 122

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -66,4 +66,4 @@ ggplot() +
 geom_raster(data = DSM_TUD_df, aes(x = x, y = y, fill = fct_elevation_cb)) +
   scale_fill_manual(values = my_col, name = "Elevation") +
   theme(axis.title = element_blank()) +
-
+coord_equal()

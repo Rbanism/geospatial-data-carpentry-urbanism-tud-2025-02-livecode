90de4d48c1b678119b26651cb970ddd688631aa2
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 16:42:47 +0100

    gitautosync update 229

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -169,5 +169,5 @@ ggplot() +
 geom_sf(data = buildings) +
 geom_sf(data = single_buffer,
         aes(fill = old_buildings_per_km2), colour = NA) +
-scale_fill_viridis_c()
+scale_fill_viridis_c(alpha = 0.6,)
 

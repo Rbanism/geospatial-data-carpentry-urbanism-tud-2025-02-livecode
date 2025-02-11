484148317d4bce8f32f0c4fc202a00a6d36b05df
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-11 10:10:46 +0100

    gitautosync update 104

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -52,4 +52,5 @@ ggplot() +
 geom_bar(data = DSM_TUD_df, aes(fct_elevation_cb))
 
 ggplot() +
-geom_raster(data = DSM_TUD_df, aes(x = x, y = y, fill = ))
+geom_raster(data = DSM_TUD_df, aes(x = x, y = y, fill = fct_elevation_cb)) +
+  coord_equal()
